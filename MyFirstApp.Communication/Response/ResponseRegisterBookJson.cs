8162bc633db0c4234549a796a4adbbc43beaaf8f
Rudinei Costa <costaaugusto177@outlook.com>
2026-01-31 19:31:59 -0300

    adicionando repositorio

--- MyFirstApp.Communication/Response/ResponseRegisterBookJson.cs
+++ MyFirstApp.Communication/Response/ResponseRegisterBookJson.cs
@@ -1,9 +1,4 @@
-﻿using MyFirstApp.Communication.Enum;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace MyFirstApp.Communication.Response
+﻿namespace MyFirstApp.Communication.Response
 {
     public class ResponseRegisterBookJson
     {
